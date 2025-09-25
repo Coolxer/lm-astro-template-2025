553eb315fae1af36288cc98ef496b9cddab9d86f
Lukasz <lukasz.milos.dev@gmail.com>
2025-09-25 10:20:53 +0200

    base conf

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,24 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
-// https://astro.build/config
-export default defineConfig({});
+import tailwindcss from "@tailwindcss/vite";
+import mdx from "@astrojs/mdx";
+import sitemap from "@astrojs/sitemap";
+
+export default defineConfig({
+  site: "https://www.example.com",
+  base: ".",
+  trailingSlash: "always",
+  output: "static",
+  redirects: {},
+
+  integrations: [mdx(), sitemap()],
+
+  vite: {
+    plugins: [tailwindcss()],
+  },
+
+  build: {
+    format: "file",
+  },
+});
